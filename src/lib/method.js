2efed4f4ac7ceccc3dcee7b0fdd78cfb87bf190a
sabikuntazina2025 <sabikuntazina@gmail.com>
2026-05-03 16:24:28 +0600

    fixed issues

--- src/lib/method.js
+++ src/lib/method.js
@@ -1,5 +1,5 @@
   export const getAllBooks= async()=>{
-      const res= await fetch('https://lendora-online-book-borrowing-platf.vercel.app/data.json', {cache: "no-store"});
+      const res= await fetch('https://lendora-online-book-borrowing-platf.vercel.app/data.json');
   const allBooks= await res.json();
  return allBooks;
 
