c0ce09d7aea01e61132d23c0a1328858c77fe3a1
sabikuntazina2025 <sabikuntazina@gmail.com>
2026-05-03 16:14:00 +0600

    fixed issues

--- src/components/BookRating.jsx
+++ src/components/BookRating.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import BookRatingsSection from './HomePage/BookRatingsSection';
+import { getAllBooks } from '@/lib/method';
 
 const BookRating =async () => {
    const books= await getAllBooks()
